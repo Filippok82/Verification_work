4e529cad54f228a89adbf731a160dade4ecb5db0
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-01-10 22:04:34 +0300

    Solution task02

---
+++ Dev2/Task02/Program.cs
@@ -0,0 +1,53 @@
+﻿//  Написать программу, которая из имеющегося массива целых чисел формирует массив из чисел, больших 8.
+
+int [] arrayA = new int[20];
+int[] FillArray(int[] array)   // Заполнение массива 
+{
+ Random random = new Random();
+    for(int i=0; i<array.Length; i++) 
+        {
+            array[i] = random.Next(-25,25);
+        } 
+    return array;
+}
+
+void PrintArray(int[] array)  // Распечатка массива
+{
+    for(int i=0; i<array.Length; i++) 
+    {
+            Console.Write($" {array[i],4}");
+        
+    }  
+    Console.WriteLine();
+}
+
+int[] SearchEven(int[] array)  // Поиск элементов больше 8 в  массиве А и помещение в массив Б
+{
+    int n = 0;
+    int j = 0;
+    for(int i=0; i<array.Length;i++)
+    {
+        if(array[i]>8) 
+        {
+            n++;
+        }
+    }
+    int[] arrayB = new int[n];
+    for(int i=0; i<array.Length;i++)
+    {
+        if(array[i]>8) 
+        {
+            arrayB[j] = array[i];      
+   
+            j++;
+        }
+    }
+    
+    return arrayB; 
+}
+
+ FillArray(arrayA);
+Console.Write("Первоначальный массив:");
+PrintArray(arrayA);
+Console.Write("Массив из элементов, больше 8:");
+PrintArray(SearchEven(arrayA)); 
